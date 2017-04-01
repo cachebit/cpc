b9538cded0952a1a76ba4763a8b50a1476b89aae
cachebit <417209993@qq.com>
2017-04-01 17:09:10 +0000

    modified the genre selector for sketch draft and setting

--- resources/views/forms/_genre_selector.blade.php
+++ resources/views/forms/_genre_selector.blade.php
@@ -1,3 +1,26 @@
+@if($type === 'sketches')
+<div class="form-group">
+  <label for="type">genre:</label>
+  <select class="form-control" name="genre">
+    <option valut="single_frames">single frames</option>
+  </select>
+</div>
+@elseif($type === 'drafts')
+<div class="form-group">
+  <label for="type">genre:</label>
+  <select class="form-control" name="genre">
+    <option value="scenarios">scenarios</option>
+  </select>
+</div>
+@elseif($type === 'settings')
+<div class="form-group">
+  <label for="type">genre:</label>
+  <select class="form-control" name="genre">
+    <option valut="single_frames">single frames</option>
+    <option value="scenarios">scenarios</option>
+  </select>
+</div>
+@else
 <div class="form-group">
   <label for="type">genre:</label>
   <select class="form-control" name="genre">
@@ -7,3 +30,4 @@
     <option value="scenarios">scenarios</option>
   </select>
 </div>
+@endif
